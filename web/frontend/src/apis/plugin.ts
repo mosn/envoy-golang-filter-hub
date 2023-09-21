10abfef09f3a6397fe8a997bf426c8cf688a38d1
NX <976180942@qq.com>
2023-09-22 00:17:08 +0800

    fix front router

--- web/frontend/src/apis/plugin.ts
+++ web/frontend/src/apis/plugin.ts
@@ -8,13 +8,15 @@ export interface PluginItem {
   version: string
 }
 
+// https://raw.githubusercontent.com/NX-Official/envoy-golang-filter-hub/main/web/cache/index.json
 export const getPluginList = async (): Promise<PluginItem[] | false> => {
-  return await fetch(baseUrl + 'pluginList')
+  return await fetch(baseUrl + 'index.json')
     .then((res) => res.json())
     .then((res) => {
-      if (res.code !== 200) throw new Error(res.message)
+      // if (res.code !== 200) throw new Error(res.message)
 
-      return res.data.plugins as PluginItem[]
+      // return res.data.plugins as PluginItem[]
+      return res.plugins as PluginItem[]
     })
     .catch((err) => {
       console.error(err)
@@ -42,13 +44,15 @@ export interface PluginDownloadItem {
   url: string
 }
 
+// https://github.com/NX-Official/envoy-golang-filter-hub/blob/main/web/cache/plugins/example.json
 export const getPlugin = async (path: string): Promise<PluginData | false> => {
-  return await fetch(baseUrl + 'plugin/' + path)
+  return await fetch(baseUrl + 'plugins/' + path + '.json')
     .then((res) => res.json())
     .then((res) => {
-      if (res.code !== 200) throw new Error(res.message)
+      // if (res.code !== 200) throw new Error(res.message)
 
-      return res.data as PluginData
+      // return res.data as PluginData
+      return res as PluginData
     })
     .catch((err) => {
       console.error(err)
--- web/frontend/src/utils/env.ts
+++ web/frontend/src/utils/env.ts
@@ -1,3 +1,4 @@
-export const baseUrl = import.meta.env.VITE_BASE_URL
+// export const baseUrl = import.meta.env.VITE_BASE_URL
+export const baseUrl = "https://ghproxy.com/https://raw.githubusercontent.com/NX-Official/envoy-golang-filter-hub/main/web/cache/"
 export const markdownFallbackUrl = import.meta.env.VITE_MARKDOWN_FALLBACK_URL
 export const isDev = import.meta.env.DEV
