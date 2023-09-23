b24055dcfef77faa302ad7def7d4cd22416c2c92
NX <976180942@qq.com>
2023-09-23 13:18:52 +0800

    fix front url

--- web/frontend/src/utils/env.ts
+++ web/frontend/src/utils/env.ts
@@ -1,3 +1,3 @@
 export const github_repo = import.meta.env.VITE_GITHUB_REPO
-export const baseUrl = "https://ghproxy.com/https://raw.githubusercontent.com/" + github_repo + "/main/web/cache/"
+export const baseUrl = "https://ghproxy.com/https://raw.githubusercontent.com/" + github_repo + "/cache/"
 export const isDev = import.meta.env.DEV
