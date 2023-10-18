8d2195de32ce6ed06aa3813ec8a469a74e571cea
纪卓志 <jizhuozhi.george@gmail.com>
2023-10-18 00:41:01 -0500

    Revert "OSPP PR"

--- web/frontend/src/utils/env.ts
+++
@@ -1,3 +0,0 @@
-export const github_repo = import.meta.env.VITE_GITHUB_REPO
-export const baseUrl = "https://ghproxy.com/https://raw.githubusercontent.com/" + github_repo + "/cache/"
-export const isDev = import.meta.env.DEV
